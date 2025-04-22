194ecd7b7f511eb8f70933b8316a68d96463dd49
sayblup <sayblup@gmail.com>
2025-04-22 12:04:52 +0200

    main.c
    wprowadzono opis i usunięto odstęp

--- main.c
+++ main.c
@@ -1,27 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+//zadanie 4
 void zamien(double *pd1, double *pd2) {
     double temp = *pd1;
     *pd1 = *pd2;
     *pd2 = temp;
 }
-
 int main() {
     int x, *px;
     int i;
     double y, *py;
     int a[5], *pa;
     double d1 = 3.14, d2 = 2.71;
-
     px = &x;
     py = &y;
     pa = a;
-
     printf("\n Podaj x:");
     scanf("%d", px);
     printf("\n x=%d", *px);
-
     printf("\n Podaj y:");
     scanf("%lf", py);
     printf("\n y=%f", *py);
